434f9506ba40b066d25777ba20e75e4805ba5c72
Lucifer <alex-orl1@mail.ru>
2021-09-28 23:55:34 +0300

    LDBR-1.14: Добавить JS линтер

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -11,9 +12,9 @@ app.use(morgan('dev'));
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/welcome.html'));
+  res.sendFile(path.join(__dirname, '../public/welcome.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
