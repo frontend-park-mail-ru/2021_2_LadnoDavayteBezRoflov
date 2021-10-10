0a772c726bc2addb9ab3603eba2a53cbc8cf2fe0
Georgiy <dev.georgiy@gmail.com>
2021-10-11 01:56:02 +0300

    LDBR 2.10: Реализовать класс URLTemplateValidator

--- src/utils/Router/URLProcessor.js
+++ src/utils/Router/URLProcessor.js
@@ -19,19 +19,7 @@ class ProcessedURLTemplate {
 
 /**
  * Данный класс используется для проверки шаблона URL, добавляемого в роутер.
- * URL должен иметь следующую структуру:
- * 1. Начинаться с "/".
- * 2. После начального "/" следует имя url - латинские символы, цифры и "/".
- *    Не может быть идущих подряд "/".
- * 3. Если url имеет часть с паттернами переменных, то имя url заканчивается на "/".
- *    Если паттерн-части url нет - url заканчивается на лат. символ или цифру.
- * 4. Паттерн часть состоит из блоков описывающих захватываемые значения.
- *    Захватываемое значение содержит как минимум 1 символ. Блоки разделяются "/".
- *    После завершающего блока "/" не ставится. Формат блоков: <type:varName>, где:
- *    - type: num или str (число (цифры) или строка (лат. символы и цифры) соответственно).
- *    - varName: имя переменной которая будет хранить соотв. значение (после обработки url).
- *      Имя переменной может состоять из лат. символов и цифр.
- * Пример шаблона: "/boards/show/<str:teamName>/<num:pageNo>", "/profile".
+ * В случае если шаблон валидный позволяет получить объект ProcessedURLTemplate.
  */
 class URLTemplateValidator {
     /** Getter, возвращающий объект с составляющими url шаблона */
@@ -39,25 +27,96 @@ class URLTemplateValidator {
         return this._processedTemplate;
     }
 
-    /** Getter, возвращает результат валидации шаблона url. */
-    get isValid() {
-        return this._isValid;
-    }
-
-    /** Возвращает причину не валидности паттерна, если processedTemplate === true */
-    get errorDescription() {
-        return this._errorDescription;
+    /**
+     * Валидирует переданный шаблон url. Результаты проверки и сообщение,
+     * (в случае не пройденной валидации) доступны в полях isValid и errorDescription.
+     * @param {string} template - проверяемый шаблон url
+     */
+    constructor(template) {
+        this._processedTemplate = new ProcessedURLTemplate();
+        this._template = template;
     }
 
     /**
-     * Валидирует переданный url. Резуьтаты проверки и сообщение,
-     * (в случае не пройденной валидации) доступны в полях isValid и errorDescription.
-     * @param {string} url - проверяемый url
+     * Разбирает строку шаблона url, строит объект ProcessedURLTemplate.
+     * Паттерн URL должен иметь следующую структуру:
+     * 1. Начинаться с "/".
+     * 2. После начального "/" следует имя url - латинские символы, цифры и "/".
+     *    Не может быть идущих подряд "/".
+     * 3. Если url имеет часть с паттернами переменных, то имя url заканчивается на "/".
+     *    Если паттерн-части url нет - url заканчивается на лат. символ или цифру.
+     * 4. Паттерн часть состоит из блоков описывающих захватываемые значения.
+     *    Захватываемое значение содержит как минимум 1 символ. Блоки разделяются "/".
+     *    После завершающего блока "/" не ставится. Формат блоков: <type:varName>, где:
+     *    - type: num или str (число (цифры) или строка (лат. символы и цифры) соответственно).
+     *    - varName: имя переменной которая будет хранить соотв. значение (после обработки url).
+     *      Имя переменной может состоять из лат. символов и цифр и не может начинаться с цифры.
+     * Пример шаблона: "/boards/show/<str:teamName>/<num:pageNo>", "/profile".
+     * @throws {Error} с описанием причины не валидности шаблона
+     * @param {string} template - строка шаблона url
      */
-    constructor(url) {
-        this._processedTemplate = new ProcessedURLTemplate();
-        this._isValid = false;
-        this._errorDescription = '';
+    validate(template) {
+        this.checkRegex(URLProcessorRegEx.SymbolSet, URLProcessorConstants.ValidateErrors.SymbolSet);
+        this.checkRegex(URLProcessorRegEx.AllExceptMoreThanOneSlash,
+                        URLProcessorConstants.ValidateErrors.AllExceptMoreThanOneSlash);
+        this.checkRegex(URLProcessorRegEx.TemplateStart,
+                        URLProcessorConstants.ValidateErrors.TemplateStart);
+        this.parseURLName();
+        this.parseURLCaptures();
+    }
+
+    // private:
+    /**
+     * Проверяет соответствует ли регулярному выражению this._template.
+     * @throws {Error} если шаблон не соответствует регулярке
+     * @param {RegExp} regex - регулярное выражения на проверку
+     * @param {string} error - описание проверки
+     */
+    checkRegex(regex, error) {
+        if (this._template.match(regex)) {
+            throw new Error(error + ` Шаблон: ${this._template}`);
+        }
+    }
+
+    /**
+     * Пытается распарсить name часть url'a и сохранить ее в ProcessedURLTemplate.name
+     * @throws {Error} если name часть не соотв. паттерну
+     */
+    parseURLName() {
+        const match = this._template.match(URLProcessorRegEx.URLName);
+        if (!match) {
+            throw new Error(`Name часть шаблона не корректна. Шаблон ${this._template}`);
+        }
+        this._processedTemplate.name = match[1];
+    }
+
+    /**
+     * Пытается распарсить шаблоны из "паттерн-части" URL и сохранить их в ProcessedURLTemplate.captures
+     * @throws {Error} если captures не соответствуют паттерну
+     */
+    parseURLCaptures() {
+        const patternURL = this._template.replace(this._processedTemplate.name, '');
+        if (patternURL === '') {
+            return; // pattern часть отсутствует
+        }
+
+        if (!patternURL.endsWith('>')) {
+            throw new Error(`Не верное окночание шаблона с параметрами. Шаблон: "${this._template}"`);
+        }
+
+        const rawPatterns = patternURL.split('/').slice(1);
+        rawPatterns.forEach((pattern) => {
+            const match = pattern.match(URLProcessorRegEx.CapturesPattern);
+            if (!match) {
+                throw new Error(`Не удалось распознать шаблон переменной "${pattern}"` +
+                                 `в шаблоне url "${this._template}"`);
+            }
+
+            this._processedTemplate.captures.push({
+                type: match[1],
+                name: match[2],
+            });
+        });
     }
 }
 
@@ -193,7 +252,7 @@ class URLProcessor {
             const match = rawParams[index].match(URLProcessorRegEx.Captures[capture.type]);
             if (!match) {
                 throw new Error(`В url "${path}" path переменная "${capture.name}" ` +
-                                `содержит не доспустимое значение "${rawParams[index]}"`);
+                    `содержит не доспустимое значение "${rawParams[index]}"`);
             }
             params[capture.name] = capture.type === URLProcessorConstants.CapturesType.number ?
                 parseInt(match[0], 10) : match[0];
@@ -205,6 +264,12 @@ class URLProcessor {
 
 const URLProcessorConstants = {
     DummyOrigin: 'https://developer.mozilla.org/', // Использую в качестве базового адреса в URL
+    ValidateErrors: {
+        SymbolSet: 'Шаблон содержит не корректные символы',
+        AllExceptMoreThanOneSlash: 'Шаблон содержит более одного идущего подряд слеша',
+        TemplateStart: 'Шаблон должен начинаться с одного "/"',
+        PatternPartEnd: 'Неправильное завершение шаблона url.',
+    },
     CapturesType: {
         string: 'str',
         number: 'num',
@@ -215,6 +280,11 @@ const URLProcessorConstants = {
 const URLProcessorRegEx = {
     ClearPathEnd: /(\/*)$/, // Слеши в конце строки
     SlashCount: /\//g, // Для поиска числа слешей
+    TemplateStart: /^\/[A-za-z0-9]+/, // Начало url: / + адекватные символы
+    AllExceptMoreThanOneSlash: /^(?!.*\/{2,}).*/, // Все кроме 2х и более слешей
+    SymbolSet: /^[A-za-z0-9\/<:>]+$/,  // Допустимые символы
+    URLName: /^(\/[A-Za-z0-9\/]+)(?:\/<|$)/, // Паттерн имени урла в ранее проверенной строке
+    CapturesPattern: /^<(str|num):([A-Za-z][A-Za-z0-9]+)>$/, // Захватываем тип и имя переменной
     Captures: {
         [URLProcessorConstants.CapturesType.string]: /^[A-Za-z0-9]+$/,
         [URLProcessorConstants.CapturesType.number]: /^[0-9]+$/,
