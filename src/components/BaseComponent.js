ea67408db3e5d9922521cb63eb75b8d0e6515893
Georgiy <dev.georgiy@gmail.com>
2021-11-06 14:26:01 +0300

    LDBR-2.29: Добавить в конструктор BaseComponent функционал из BaseView и параметры под PopUp

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -5,13 +5,22 @@ export default class BaseComponent {
     /**
      * @constructor
      * @param {Object} context контекст отрисовки шаблона
-     * @param {Function} template функция отрисовки шаблона
-     * @param {Element?} parent элемент, в который будет отрисован компонент
+     * @param {Object} mainTemplate объект с функцией шаблона и контейнером для него
+     * @param {Function} mainTemplate.template функция отрисовки шаблона
+     * @param {Element?} mainTemplate.parent элемент, в который будет отрисован шаблон
+     * @param {Object} popupTemplate Объект с функцией шаблона popup и контейнером для него
+     * @param {Function} popupTemplate.template функция отрисовки шаблона popup
+     * @param {Element?} popupTemplate.parent элемент, в который будет отрисован шаблон popup
      */
-    constructor(context, template, parent) {
-        this.parent = parent;
-        this.template = template;
+    constructor(context, mainTemplate, popupTemplate) {
+        if (!mainTemplate.template) {
+            throw new Error('Не задан основной шаблон компонента');
+        }
+
+        this.mainTemplate = mainTemplate;
+        this.popupTemplate = popupTemplate;
         this.context = context;
+        this.subComponents = [];
     }
 
     /**
@@ -19,6 +28,13 @@ export default class BaseComponent {
      * @return {String} HTML-код компонента
      */
     render() {
+        const components = this.subComponents.reduce(function(accumulator, object) {
+            return {...accumulator, ...{[object[0]]: object[1].render()}};
+        }, {});
+
+        this.parent.innerHTML = this.template({...components, ...Object.fromEntries(this.context)});
+
+
         if (!!this.template) {
             if (this.context instanceof Map) {
                 this.context = Object.fromEntries(this.context);
@@ -39,11 +55,17 @@ export default class BaseComponent {
      * Метод, добавляющий обработчики событий для компонента.
      */
     addEventListeners() {
+        this.subComponents.forEach(([_, component]) => {
+            component.addEventListeners();
+        });
     }
 
     /**
      * Метод, удаляющий обработчики событий для компонента.
      */
     removeEventListeners() {
+        this.subComponents.forEach(([_, component]) => {
+            component.removeEventListeners();
+        });
     }
 }
