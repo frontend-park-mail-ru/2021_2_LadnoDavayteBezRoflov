694a93aedac6681d53276336febb4cdca238e43c
Georgiy <dev.georgiy@gmail.com>
2021-11-15 23:13:11 +0400

    LDBR-3.1: Переверстать navbar

--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -106,7 +106,7 @@ export default class LoginView extends BaseView {
     removeEventListeners() {
         super.removeEventListeners();
         document.getElementById('auth')?.removeEventListener('submit',
-            this.formAuthorizationCallback);
+                                                             this.formAuthorizationCallback);
     }
 
     /**
