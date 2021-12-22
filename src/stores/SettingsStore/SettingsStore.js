5328908e1b03a4acb2e995b57147f31fded44c4c
Dmitry Peshkov <31134795+DPeshkoff@users.noreply.github.com>
2021-12-22 17:36:55 +0300

    Revert "LDBR-4.49: Поправить обновление аватара при загрузке (#57)"
    This reverts commit 4a75be25e3fc8adad4e298292f9cc913924d97b5.

--- src/stores/SettingsStore/SettingsStore.js
+++ src/stores/SettingsStore/SettingsStore.js
@@ -252,15 +252,11 @@ class SettingsStore extends BaseStore {
             }
         }
 
-        const avatar = this.__setAvatar(data.avatar);
+        this._storage.set('avatar', this.__setAvatar(data.avatar));
 
-        if (avatar) {
-            this._storage.set('avatar', avatar);
+        const formdata = new FormData();
 
-            const formdata = new FormData();
-
-            formdata.append('avatar', data.avatar);
-        }
+        formdata.append('avatar', data.avatar);
 
         let payload;
 
